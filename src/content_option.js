8e389c8d19072f15580fb54b272a03bd51b41332
vianchi <vianchi.nieto@gmail.com>
2024-08-04 21:39:44 -0500

    Prueba de cambios 1
    Cambio de nombre y titulo

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
-const logotext = "JOHN";
+const logotext = "David";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "David Nieto",
+    description: "Ingeniero de software con una amplia experiencia en el sector TI.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "David",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Soy Ingeniero",
+        second: "de Software",
+        third: "Especialista en seguridad y gestion de bases de datos",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
